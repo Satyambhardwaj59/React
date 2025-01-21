4f98d8a4957c2f0c7a918f033c93a261f86143bd
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-01-21 22:05:52 +0530

    adding cealr card option in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,31 @@
 import ItemList from "./ItemList";
 import { useSelector } from "react-redux";
+import { useDispatch } from 'react-redux';
+import { clearCart} from '../utils/cartSlice';
 
 
 const Cart = () => {
     const cartItems = useSelector((store) => store?.cart);
+
+    const dispatch = useDispatch();
+
+    const handleClaerCart = () => {
+        dispatch(clearCart());
+    };
     
     return (
         <div className="my-5 p-4">
             <h1 className="text-center text-2xl font-bold">Cart</h1>
+            
             <div className="w-6/12 mx-auto">
-              <ItemList items={cartItems?.items}/>
+                <div className="flex justify-end ">
+                    <button className="border-2 bg-orange-400 text-white p-2 rounded-lg text-xl" onClick={handleClaerCart}>Clear Cart</button>
+                </div>
+                <div>
+                    {cartItems.items.length === 0 &&  <h1 className="text-center text-2xl bg-gray-200 m-4 p-4 rounded-lg">Your card is empty, Please Add some item!!! </h1>}
+                    <ItemList items={cartItems.items}/> 
+                </div>
+               
             </div>
             
         </div>

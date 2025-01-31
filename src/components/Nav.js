aeff3f9d4553975611072ccb1f05819792208b84
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-01-31 21:41:10 +0530

    trying responsive Header

---
+++ src/components/Nav.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+
+const Nav = () => {
+    let Links= [
+        {name: "Home", path: "/"},
+        {name: "About", path: "/about"},
+        {name: "Contact", path: "/contact"},
+        {name: "Grocary", path: "/grocary"},
+        {name: "Cart", path: "/cart"},
+    ];
+
+    const [showMenu, setShowMenu] = useState(false);
+    return (
+        <div className="shadow-md fixed top-0 w-11/12">
+            <div className="md:flex justify-between bg-gray-200 py-4">
+                <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-black">
+                        <h1>Hello</h1>
+                </div>
+                {/* <div onClick={ () => !setShowMenu(true)} className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden">
+                    {showMenu ? '✖' : '☰'}
+                </div> */}
+                <div className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden">
+                    <ion-icon name="menu"></ion-icon>
+                </div>
+                <ul className="md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-blue-800 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-10 transition-all duration-500 ease-in">
+                    {Links.map((link, index) => {
+                        return (
+                            <li key={index} className="md:ml-8 text-xl px-4 py-2 md:my-0 my-7">
+                                <a href={link.path} className="text-black font-[Poppins] font-bold hover:text-blue-500">{link.name}</a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+
+        </div>
+    );
+}
+
+export default Nav;

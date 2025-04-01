5fd8033b8fab48411fb037230779ea04e1f70f9d
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-04-01 21:42:31 +0530

    temprory fix CORS issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,10 +4,10 @@ export const URL_MOOK = "https://media-assets.swiggy.com/swiggy/image/upload/fl_
 
 export const URL_Err = "https://traqq.com/blog/wp-content/uploads/2020/04/qwer_edited-1024x473.jpg";
 
-export const URL_RES_DATA =  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId="
+export const URL_RES_DATA =  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId="
 
  // export const URL_RESTAURANT_CARD = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
- export const URL_RESTAURANT_CARD = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.612677&lng=85.158875&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+ export const URL_RESTAURANT_CARD = "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.612677&lng=85.158875&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
 export const BANNER_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/seo/Food_collectionbanner.png";
